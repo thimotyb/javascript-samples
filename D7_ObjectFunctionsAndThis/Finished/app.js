97410c8326748152c6797c7c339b70d6567ee4f0
thimotyb <thimoty@thimoty.it>
2025-06-04 11:48:43 +0200

    Update app.js

--- D7_ObjectFunctionsAndThis/Finished/app.js
+++ D7_ObjectFunctionsAndThis/Finished/app.js
@@ -31,6 +31,47 @@ var c = {
 
 c.log();
 
+var d = {
+    name: 'The d object',
+    log: function() {
+        this.name = this.name + 'Updated d object';
+        console.log(this);
+    }
+}
+
+var e = {
+    name: 'The e object',
+    log: function() {
+
+        this.name = this.name + 'Updated e object';
+        console.log(this);
+
+        var setname = function(newname) {
+            this.name = newname;   
+        }
+        setname('Updated with setname! By the e object');
+        console.log(this);
+
+    }
+}
+
+var f = {
+    name: 'The f object',
+    log: function() {
+
+        this.name = this.name + 'Updated f object';
+        console.log(this);
+
+        // What happens to this if I am defining an arrow function?
+        var setname = (newname) => {
+            this.name = newname;   
+        }
+        setname('Updated with setname! By the f object');
+        console.log(this);
+
+    }
+}
+
 
 
 
